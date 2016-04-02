cc25a8d891c311a3cf580c6a70de6e1069c389ea
Lucas Hoezee <luke.hoezee@gmail.com>
2016-04-02 00:07:22 -0400

    Updated directory structure for auto loading

---
+++ src/CodifyLLC/softaculous/softaculous.php
@@ -0,0 +1,260 @@
+<?php
+
+namespace CodifyLLC\softaculous;
+
+class softaculous {
+    private $user;
+    private $password;
+    private $domain;
+    private $port;
+    private $timeout;
+    private $act;
+    private $soft;
+    private $insid;
+    private $post;
+    private $curlDebug;
+
+    public function __construct()
+    {
+        $this->setPort(2083);
+        $this->setTimeout(60);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDomain()
+    {
+        return $this->domain;
+    }
+
+    /**
+     * @param mixed $domain
+     */
+    public function setDomain($domain)
+    {
+        $this->domain = $domain;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * @param mixed $port
+     */
+    public function setPort($port)
+    {
+        $this->port = $port;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTimeout()
+    {
+        return $this->timeout;
+    }
+
+    /**
+     * @param mixed $timeout
+     */
+    public function setTimeout($timeout)
+    {
+        $this->timeout = $timeout;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAct()
+    {
+        return $this->act;
+    }
+
+    /**
+     * @param mixed $act
+     */
+    public function setAct($act)
+    {
+        $this->act = $act;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSoft()
+    {
+        return $this->soft;
+    }
+
+    /**
+     * @param mixed $soft
+     */
+    public function setSoft($soft)
+    {
+        $this->soft = $soft;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInsid()
+    {
+        return $this->insid;
+    }
+
+    /**
+     * @param mixed $insid
+     */
+    public function setInsid($insid)
+    {
+        $this->insid = $insid;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getPost()
+    {
+        return $this->post;
+    }
+
+    /**
+     * @param softaculousPost $post
+     */
+    public function setPost(softaculousPost $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurlDebug()
+    {
+        return $this->curlDebug;
+    }
+
+    /**
+     * @param mixed $curlDebug
+     */
+    public function setCurlDebug($curlDebug)
+    {
+        $this->curlDebug = $curlDebug;
+    }
+
+    
+
+
+    public function send()
+    {
+        $url = 'https://' . $this->getUser() .':'.$this->getPassword().'@'.$this->getDomain().':'.$this->getPort();
+        $url .= '/frontend/paper_lantern/softaculous/index.live.php?&api=serialize';
+
+        $act = $this->getAct();
+        if (!empty($act)) {
+            $url .= '&act=' . $act;
+        }
+
+        $soft = $this->getSoft();
+        if (!empty($soft)) {
+            $url .= '&soft=' . $soft;
+        }
+
+        $insid = $this->getInsid();
+        if (!empty($insid)) {
+            $url .= '&insid=' . $insid;
+        }
+
+        print $url;
+        print "\n\n";
+
+
+        // Set the curl parameters
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->getTimeout());
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        if(!empty($this->post)) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+
+            $post = get_object_vars($this->post);
+            foreach($post as $key => $val) {
+                if (empty($val)) {
+                    unset($post[$key]);
+                }
+            }
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+        }
+
+
+        // CURLOPT_VERBOSE: TRUE to output verbose information. Writes output to STDERR,
+        // or the file specified using CURLOPT_STDERR.
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
+
+        $verbose = fopen('php://temp', 'rw+');
+        curl_setopt($ch, CURLOPT_STDERR, $verbose);
+
+        // Get response from the server.
+        $resp = curl_exec($ch);
+
+        // close cURL resource, and free up system resources
+        curl_close($ch);
+
+        rewind($verbose);
+        $verboseLog = stream_get_contents($verbose);
+
+        if ($this->getCurlDebug()) {
+
+            print "Curl Debug: \n";
+            print $verboseLog;
+        }
+
+
+
+        print $resp;
+        // Unserialize data
+        $res = unserialize($resp);
+
+        return $res;
+    }
+}
---
+++ src/CodifyLLC/softaculous/softaculousPost.php
@@ -0,0 +1,228 @@
+<?php
+namespace CodifyLLC\softaculous;
+
+class softaculousPost {
+    public $softsubmit;
+    public $softdomain;
+    public $softdirectory;
+    public $softdb;
+    public $dbusername;
+    public $dbuserpass;
+    public $admin_username;
+    public $admin_pass;
+    public $admin_email;
+    public $language;
+    public $site_name;
+    public $site_desc;
+    public $noemail;
+
+    /**
+     * @return mixed
+     */
+    public function getSoftsubmit()
+    {
+        return $this->softsubmit;
+    }
+
+    /**
+     * @param mixed $softsubmit
+     */
+    public function setSoftsubmit($softsubmit)
+    {
+        $this->softsubmit = $softsubmit;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSoftdomain()
+    {
+        return $this->softdomain;
+    }
+
+    /**
+     * @param mixed $softdomain
+     */
+    public function setSoftdomain($softdomain)
+    {
+        $this->softdomain = $softdomain;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSoftdirectory()
+    {
+        return $this->softdirectory;
+    }
+
+    /**
+     * @param mixed $softdirectory
+     */
+    public function setSoftdirectory($softdirectory)
+    {
+        $this->softdirectory = $softdirectory;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSoftdb()
+    {
+        return $this->softdb;
+    }
+
+    /**
+     * @param mixed $softdb
+     */
+    public function setSoftdb($softdb)
+    {
+        $this->softdb = $softdb;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDbusername()
+    {
+        return $this->dbusername;
+    }
+
+    /**
+     * @param mixed $dbusername
+     */
+    public function setDbusername($dbusername)
+    {
+        $this->dbusername = $dbusername;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDbuserpass()
+    {
+        return $this->dbuserpass;
+    }
+
+    /**
+     * @param mixed $dbuserpass
+     */
+    public function setDbuserpass($dbuserpass)
+    {
+        $this->dbuserpass = $dbuserpass;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdminUsername()
+    {
+        return $this->admin_username;
+    }
+
+    /**
+     * @param mixed $admin_username
+     */
+    public function setAdminUsername($admin_username)
+    {
+        $this->admin_username = $admin_username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdminPass()
+    {
+        return $this->admin_pass;
+    }
+
+    /**
+     * @param mixed $admin_pass
+     */
+    public function setAdminPass($admin_pass)
+    {
+        $this->admin_pass = $admin_pass;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdminEmail()
+    {
+        return $this->admin_email;
+    }
+
+    /**
+     * @param mixed $admin_email
+     */
+    public function setAdminEmail($admin_email)
+    {
+        $this->admin_email = $admin_email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param mixed $language
+     */
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSiteName()
+    {
+        return $this->site_name;
+    }
+
+    /**
+     * @param mixed $site_name
+     */
+    public function setSiteName($site_name)
+    {
+        $this->site_name = $site_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSiteDesc()
+    {
+        return $this->site_desc;
+    }
+
+    /**
+     * @param mixed $site_desc
+     */
+    public function setSiteDesc($site_desc)
+    {
+        $this->site_desc = $site_desc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNoemail()
+    {
+        return $this->noemail;
+    }
+
+    /**
+     * @param mixed $noemail
+     */
+    public function setNoemail($noemail)
+    {
+        $this->noemail = $noemail;
+    }
+
+    
+}
